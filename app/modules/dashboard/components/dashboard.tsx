4f57b6e021cdf0cae0c3f569357eac8528a665ae
darylhedley <darylhedley@hotmail.com>
2025-06-16 10:14:30 +0100

    Adds basic setup to render frontend

---
+++ app/modules/dashboard/components/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Projects } from "~/modules/projects/components/projects";
+
+export function Dashboard() {
+  return (
+    <main>
+      Dashboard
+      <Projects />
+    </main>
+  );
+}
---
+++ app/modules/projects/components/projects.tsx
@@ -0,0 +1,7 @@
+export function Projects() {
+  return (
+    <div className="flex items-center justify-center pt-16 pb-4">
+      Set of projects to be rendered here
+    </div>
+  );
+}
