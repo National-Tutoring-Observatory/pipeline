0035a2b38857252dcdbdd5d574e544e2551d2a43
darylhedley <darylhedley@hotmail.com>
2025-06-26 10:05:58 +0100

    Adds files and tabbing of runs to files

--- app/modules/projects/components/project.tsx
+++ app/modules/projects/components/project.tsx
@@ -1,21 +1,63 @@
 import UploadFilesContainer from "~/modules/files/containers/uploadFiles.container";
 import type { Project } from "../projects.types";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Link, Outlet } from "react-router";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import clsx from "clsx";
 
 export default function Project({
   project,
+  tabValue,
   onUploadFiles
-}: { project: Project, onUploadFiles: (acceptedFiles: any[]) => void }) {
+}: { project: Project, tabValue: string, onUploadFiles: (acceptedFiles: any[]) => void }) {
 
   return (
     <div className="max-w-5xl mx-auto p-8">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance mb-8">
         {project.name}
       </h1>
-      <div>
-        <UploadFilesContainer
-          onUploadFiles={onUploadFiles}
-        />
-      </div>
+      {(!project.hasSetupProject) && (
+        <div>
+          <UploadFilesContainer
+            onUploadFiles={onUploadFiles}
+          />
+        </div>
+      )}
+      {(project.hasSetupProject) && (
+        <div>
+          <div className="grid grid-cols-2 gap-4">
+            <Link to={`/projects/${project._id}`} >
+              <Card className={clsx({
+                "border-accent-foreground": tabValue === 'RUNS'
+              })}>
+                <CardHeader>
+                  <CardTitle>Runs</CardTitle>
+                  <CardDescription>Runs are a way to annotate your data via Prompts</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>0 runs</p>
+                </CardContent>
+              </Card>
+            </Link>
+            <Link to={`/projects/${project._id}/files`}>
+              <Card className={clsx({
+                "border-accent-foreground": tabValue === 'FILES'
+              })}>
+                <CardHeader>
+                  <CardTitle>Files</CardTitle>
+                  <CardDescription>Files are your data files</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>30 files</p>
+                </CardContent>
+              </Card>
+            </Link>
+          </div>
+          <div>
+            <Outlet />
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- app/routes.ts
+++ app/routes.ts
@@ -5,6 +5,13 @@ export default [
     index("modules/projects/containers/projects.route.tsx")
   ]),
   ...prefix("projects", [
-    route(":id", "modules/projects/containers/project.route.tsx"),
+    route(":id", "modules/projects/containers/project.route.tsx", [
+      index("modules/projects/containers/projectRuns.route.tsx", {
+        id: "RUNS"
+      }),
+      route("files", "modules/projects/containers/projectFiles.route.tsx", {
+        id: "FILES"
+      })
+    ])
   ]),
 ] satisfies RouteConfig;
