c3fcd407931cdbb862b5a5851569d10f36a7d886
darylhedley <darylhedley@hotmail.com>
2025-07-02 16:33:04 +0100

    Saves prompt version

--- app/modules/prompts/containers/promptEditor.route.tsx
+++ app/modules/prompts/containers/promptEditor.route.tsx
@@ -2,7 +2,8 @@ import getDocument from "~/core/documents/getDocument";
 import PromptEditor from "../components/promptEditor";
 import type { Route } from "./+types/promptEditor.route";
 import type { PromptVersion } from "../prompts.types";
-import { useLoaderData } from "react-router";
+import { useLoaderData, useNavigation, useSubmit } from "react-router";
+import updateDocument from "~/core/documents/updateDocument";
 
 export async function loader({ params }: Route.LoaderArgs) {
 
@@ -11,15 +12,45 @@ export async function loader({ params }: Route.LoaderArgs) {
   return { promptVersion };
 }
 
+export async function action({
+  request,
+  params,
+}: Route.ActionArgs) {
+
+  const { intent, entityId, payload = {} } = await request.json()
+
+  const { name, userPrompt } = payload;
+
+  switch (intent) {
+    case 'UPDATE_PROMPT_VERSION':
+      const promptVersion = await updateDocument({
+        collection: 'promptVersions',
+        match: { _id: Number(entityId) },
+        update: { name, userPrompt }
+      }) as { data: PromptVersion }
+      return {};
+    default:
+      return {};
+  }
+}
+
 export default function PromptEditorRoute() {
 
   const data = useLoaderData();
+  const navigation = useNavigation();
+  const submit = useSubmit();
 
   const { promptVersion } = data;
+
+  const onSavePromptVersion = ({ name, userPrompt, _id }: { name: string, userPrompt: string, _id: string }) => {
+    submit(JSON.stringify({ intent: 'UPDATE_PROMPT_VERSION', entityId: _id, payload: { name, userPrompt } }), { method: 'PUT', encType: 'application/json' });
+  }
 
   return (
     <PromptEditor
       promptVersion={promptVersion.data}
+      isLoading={navigation.state === 'loading'}
+      onSavePromptVersion={onSavePromptVersion}
     />
   )
 }
