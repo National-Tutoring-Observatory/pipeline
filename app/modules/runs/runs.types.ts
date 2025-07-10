f66802f3e7f03d3febeb269cd28f2df60c3997e5
darylhedley <darylhedley@hotmail.com>
2025-07-10 14:10:53 +0100

    Adds started and finished at dates

--- app/modules/runs/runs.types.ts
+++ app/modules/runs/runs.types.ts
@@ -1,16 +1,18 @@
 export interface Run {
   _id: string;
   name: string;
-  createdAt: string;
   project: string;
   annotationType: string;
-  hasSetup: boolean;
-  isRunning: boolean;
   prompt: number;
   promptVersion: number;
   model: string;
   sessions: { sessionId: number, status: string, name: string, fileType: string, startedAt: Date, finishedAt: Date }[]
+  hasSetup: boolean;
+  isRunning: boolean;
   isComplete: boolean;
+  createdAt: string;
+  startedAt: string;
+  finishedAt: string;
 }
 
 export interface CreateRun {
