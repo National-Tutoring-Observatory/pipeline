6c342bf04bb0e856dc0fc94c218dc2d88f1d3e07
darylhedley <darylhedley@hotmail.com>
2025-07-16 15:35:45 +0100

    Sorts list to be reversed

--- app/modules/prompts/containers/promptVersionsList.route.tsx
+++ app/modules/prompts/containers/promptVersionsList.route.tsx
@@ -8,6 +8,6 @@ type PromptVersions = {
 export async function loader({ request, params }: Route.LoaderArgs) {
   const url = new URL(request.url);
   const prompt = url.searchParams.get('prompt');
-  const promptVersions = await getDocuments({ collection: 'promptVersions', match: { prompt: Number(prompt) }, sort: {} }) as PromptVersions;
+  const promptVersions = await getDocuments({ collection: 'promptVersions', match: { prompt: Number(prompt) }, sort: { version: -1 } }) as PromptVersions;
   return { promptVersions };
 }
