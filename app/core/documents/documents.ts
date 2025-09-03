3eba1eb8799655b508090b0167c164cfc31e09a7
darylhedley <darylhedley@hotmail.com>
2025-09-03 13:49:03 +0100

    Adds a way to register models

--- app/core/documents/documents.ts
+++ app/core/documents/documents.ts
@@ -1,3 +1,14 @@
 
 import 'app/documentsAdapters/documentDB/index.ts';
 import 'app/documentsAdapters/local/index.ts';
+
+import mongoose from 'mongoose';
+import projectSchema from './schemas/project.schema';
+
+const registerModels = () => {
+  if (!mongoose.models.Project) {
+    mongoose.model('Project', projectSchema);
+  }
+}
+
+registerModels();
