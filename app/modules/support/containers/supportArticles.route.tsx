230ddb485656ad3bf1f0d4ddf6091b2df4e53b65
darylhedley <darylhedley@hotmail.com>
2025-09-10 14:36:04 +0100

    Fetches support articles

--- app/modules/support/containers/supportArticles.route.tsx
+++ app/modules/support/containers/supportArticles.route.tsx
@@ -13,20 +13,21 @@ export async function loader({ request }: Route.LoaderArgs) {
   for (const document of documentsInDirectory) {
     const markdownContent = fs.readFileSync(path.join('./documentation', document), 'utf-8');
     const data = matter(markdownContent);
-    console.log(data);
+
     if (Object.keys(data.data).length > 0) {
-      console.log(data);
+
+      const ast = Markdoc.parse(data.content);
+
+      const content = Markdoc.transform(ast);
+
+      const html = Markdoc.renderers.html(content);
+
+      supportArticles.push({
+        documentId: document,
+        data: data.data,
+        html
+      })
     }
-    const ast = Markdoc.parse(data.content);
-
-    const content = Markdoc.transform(ast);
-
-    const html = Markdoc.renderers.html(content);
-
-    supportArticles.push({
-
-      html
-    })
   }
 
   return {
