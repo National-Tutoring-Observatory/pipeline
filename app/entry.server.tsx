cc1bdf23e938d47cac9e7b4f6bc90c09b6e3e00d
darylhedley <darylhedley@hotmail.com>
2025-10-03 16:10:57 +0100

    Trying full test for github user

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -14,19 +14,20 @@ import type { User } from "./modules/users/users.types";
 
 const checkSuperAdminExists = async () => {
   const documents = getDocumentsAdapter()
-  const user = await documents.getDocument({ collection: 'users', match: { role: 'SUPER_ADMIN' } }) as { data: User | undefined };
-  console.log(user.data);
-  if (!user.data) {
-    await documents.createDocument({
-      collection: 'users',
-      update: {
-        role: 'SUPER_ADMIN',
-        username: 'local',
-        githubId: process.env.SUPER_ADMIN_GITHUB_ID,
-        hasGithubSSO: process.env.SUPER_ADMIN_GITHUB_ID ? true : false
-      }
-    })
-  }
+  const users = await documents.getDocuments({ collection: 'users', match: { role: 'SUPER_ADMIN' } }) as { data: User | undefined };
+  console.log(users.data);
+  //if (!user.data) {
+  await documents.createDocument({
+    collection: 'users',
+    update: {
+      role: 'SUPER_ADMIN',
+      username: 'local',
+      githubId: process.env.SUPER_ADMIN_GITHUB_ID,
+      hasGithubSSO: process.env.SUPER_ADMIN_GITHUB_ID ? true : false,
+      isRegistered: true
+    }
+  })
+  //}
 }
 
 setTimeout(() => {
