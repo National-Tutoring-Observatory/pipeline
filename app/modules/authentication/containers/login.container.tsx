65247e7afcfc46c02a5d655b546021d1f1fdc654
darylhedley <darylhedley@hotmail.com>
2025-10-08 13:32:23 +0100

    Adds error messages for login screen

--- app/modules/authentication/containers/login.container.tsx
+++ app/modules/authentication/containers/login.container.tsx
@@ -1,10 +1,28 @@
-import { useFetcher } from "react-router";
+import { useFetcher, useLocation, useSearchParams } from "react-router";
 import Login from "../components/login";
 import { useEffect } from "react";
+
+const ERROR_MESSAGES = {
+  EXPIRED_INVITE: {
+    title: 'Your invite link has expired',
+    description: 'Please reach out to your NTO contact.'
+  },
+  UNREGISTERED: {
+    title: 'You have not been registered',
+    description: "Please use the 'Sign up to be invited' button below to register your interest.",
+  }
+};
+
+type ErrorType = keyof typeof ERROR_MESSAGES;
 
 export default function LoginContainer() {
 
   const fetcher = useFetcher();
+
+  const [searchParams] = useSearchParams();
+
+  const hasError = searchParams.has('error');
+  const errorType = searchParams.get('error');
 
   const onLoginWithOrcidClicked = () => {
     fetcher.submit({ provider: 'orcid' }, {
@@ -21,9 +39,24 @@ export default function LoginContainer() {
       encType: "application/json"
     });
   }
+  let errorTitle: string | undefined;
+  let errorDescription: string | undefined;
+
+  if (hasError) {
+    if (errorType && (errorType in ERROR_MESSAGES)) {
+      const error = ERROR_MESSAGES[errorType as ErrorType];
+      if (error) {
+        errorTitle = error.title;
+        errorDescription = error.description;
+      }
+    }
+  }
 
   return (
     <Login
+      errorTitle={errorTitle}
+      errorDescription={errorDescription}
+      hasError={hasError}
       onLoginWithOrcidClicked={onLoginWithOrcidClicked}
       onLoginWithGithubClicked={onLoginWithGithubClicked}
     />
