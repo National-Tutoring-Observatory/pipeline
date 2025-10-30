941a05ecf56ec010c664fd64e86654c68618da9f
Jorge Dias <jorge@freshcognate.com>
2025-10-30 08:54:52 -0400

    Add authentication for events
    Fixes #660

--- app/modules/events/containers/events.route.tsx
+++ app/modules/events/containers/events.route.tsx
@@ -1,7 +1,13 @@
-import type { LoaderFunctionArgs } from "react-router";
+import { redirect, type LoaderFunctionArgs } from "react-router";
+import getSessionUser from "~/modules/authentication/helpers/getSessionUser";
 import { emitter } from "../emitter";
 
 export async function loader({ request }: LoaderFunctionArgs) {
+
+  const user = await getSessionUser({ request });
+  if (!user) {
+    return redirect('/');
+  }
 
   const stream = new ReadableStream({
     start(controller) {
