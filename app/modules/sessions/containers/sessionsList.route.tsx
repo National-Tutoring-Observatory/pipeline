a4df82a3281f71080acdc1251273ed7107a919ba
Jorge Dias <jorge@freshcognate.com>
2025-10-30 09:32:36 -0400

    Add authentication for sessions
    Fixes #664

--- app/modules/sessions/containers/sessionsList.route.tsx
+++ app/modules/sessions/containers/sessionsList.route.tsx
@@ -1,15 +1,32 @@
+import { redirect } from "react-router";
+import getSessionUser from "~/modules/authentication/helpers/getSessionUser";
+import getDocumentsAdapter from "~/modules/documents/helpers/getDocumentsAdapter";
+import { isProjectOwner } from "~/modules/projects/helpers/projectOwnership";
+import type { User } from "~/modules/users/users.types";
 import type { Session } from "../sessions.types";
 import type { Route } from "./+types/sessionsList.route";
-import getDocumentsAdapter from "~/modules/documents/helpers/getDocumentsAdapter";
 
 type Sessions = {
   data: [Session],
 };
 
-
 export async function loader({ request }: Route.LoaderArgs) {
+  const user = await getSessionUser({ request }) as User;
+  if (!user) {
+    return redirect('/');
+  }
+
   const url = new URL(request.url);
   const project = url.searchParams.get('project');
+
+  if (!project) {
+    return redirect('/');
+  }
+
+  if (!(await isProjectOwner({ user, projectId: project }))) {
+    return redirect('/');
+  }
+
   const documents = getDocumentsAdapter();
   const sessions = await documents.getDocuments({ collection: 'sessions', match: { project: project, hasConverted: true }, sort: {} }) as Sessions;
   return { sessions };
