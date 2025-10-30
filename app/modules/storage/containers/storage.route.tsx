f53e7aaae109734127a19766c2aa799946ddad2a
Jorge Dias <jorge@freshcognate.com>
2025-10-30 09:44:12 -0400

    Add authentication for storage
    Fixes #665

--- app/modules/storage/containers/storage.route.tsx
+++ app/modules/storage/containers/storage.route.tsx
@@ -1,7 +1,13 @@
-import type { ActionFunctionArgs, LoaderFunctionArgs } from "react-router";
+import type { ActionFunctionArgs } from "react-router";
+import getSessionUser from "~/modules/authentication/helpers/getSessionUser";
+import type { User } from "~/modules/users/users.types";
 import getStorageAdapter from "../helpers/getStorageAdapter";
 
 export async function action({ request }: ActionFunctionArgs) {
+  const user = await getSessionUser({ request }) as User;
+  if (!user) {
+    throw new Error("Authentication required");
+  }
 
   const { intent, payload } = await request.json()
 
