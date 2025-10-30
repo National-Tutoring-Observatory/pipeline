38b6aface32f22be46e444294b3fa03ecd4c83e9
Jorge Dias <jorge@freshcognate.com>
2025-10-30 09:32:36 -0400

    Add authentication for sessions
    Fixes #664

--- app/modules/projects/containers/projectSessions.route.tsx
+++ app/modules/projects/containers/projectSessions.route.tsx
@@ -1,16 +1,28 @@
-import { useLoaderData } from "react-router";
-import ProjectSessions from "../components/projectSessions";
-import type { Session } from "~/modules/sessions/sessions.types";
-import type { Route } from "./+types/projectSessions.route";
+import { redirect, useLoaderData } from "react-router";
+import getSessionUser from "~/modules/authentication/helpers/getSessionUser";
 import addDialog from "~/modules/dialogs/addDialog";
-import ViewSessionContainer from "~/modules/sessions/containers/viewSessionContainer";
 import getDocumentsAdapter from "~/modules/documents/helpers/getDocumentsAdapter";
+import ViewSessionContainer from "~/modules/sessions/containers/viewSessionContainer";
+import type { Session } from "~/modules/sessions/sessions.types";
+import type { User } from "~/modules/users/users.types";
+import ProjectSessions from "../components/projectSessions";
+import { isProjectOwner } from "../helpers/projectOwnership";
+import type { Route } from "./+types/projectSessions.route";
 
 type Sessions = {
   data: [Session],
 };
 
-export async function loader({ params }: Route.LoaderArgs) {
+export async function loader({ request, params }: Route.LoaderArgs) {
+  const user = await getSessionUser({ request }) as User;
+  if (!user) {
+    return redirect('/');
+  }
+
+  if (!(await isProjectOwner({ user, projectId: params.id }))) {
+    return redirect('/');
+  }
+
   const documents = getDocumentsAdapter();
   const sessions = await documents.getDocuments({ collection: 'sessions', match: { project: params.id }, sort: {} }) as Sessions;
   return { sessions };
