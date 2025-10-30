d9e2faf3927f7f290e75bd6395c41c7631a0acd4
Jorge Dias <jorge@freshcognate.com>
2025-10-30 07:01:22 -0400

    Refactor runOwnership

--- app/modules/runs/helpers/runOwnership.ts
+++ app/modules/runs/helpers/runOwnership.ts
@@ -1,5 +1,5 @@
 import getDocumentsAdapter from "~/modules/documents/helpers/getDocumentsAdapter";
-import { isProjectOwner, validateProjectOwnership } from "~/modules/projects/helpers/projectOwnership";
+import { isProjectOwner } from "~/modules/projects/helpers/projectOwnership";
 import type { User } from "~/modules/users/users.types";
 
 export async function isRunOwner({
@@ -30,16 +30,7 @@ export async function validateRunOwnership({
     user: User;
     runId: string;
 }) {
-    const documents = getDocumentsAdapter();
-
-    const run = await documents.getDocument({
-        collection: 'runs',
-        match: { _id: runId },
-    }) as { data: { project: string } | null };
-
-    if (!run.data) {
-        throw new Error("The run does not exist.");
+    if (!(await isRunOwner({ user, runId }))) {
+        throw new Error("You do not have permission to access this run.");
     }
-
-    await validateProjectOwnership({ user, projectId: run.data.project });
 }
