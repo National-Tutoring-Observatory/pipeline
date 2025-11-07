eda8e8311293a9a0ad93437123637b710ae0de8c
darylhedley <darylhedley@hotmail.com>
2025-11-07 10:46:27 +0000

    Pings events from workers

--- workers/tasks/convertedFilesToSessions.ts
+++ workers/tasks/convertedFilesToSessions.ts
@@ -1,9 +1,20 @@
+import getSockets from "workers/helpers/getSockets";
 import getDocumentsAdapter from "~/modules/documents/helpers/getDocumentsAdapter";
 
 export default async function convertedFilesToSessions(job: any) {
+
   const { projectId } = job.data;
 
   const documents = getDocumentsAdapter();
 
   await documents.updateDocument({ collection: 'projects', match: { _id: projectId }, update: { isConvertingFiles: false } });
+
+  const sockets = await getSockets();
+
+  sockets.emit('CONVERT_FILES_TO_SESSIONS', {
+    projectId,
+    task: 'CONVERTED_FILES_TO_SESSIONS',
+    status: 'FINISHED'
+  });
+
 }
