d4bbcb889339cf126abaf00d1b195f11852cd7e7
Jorge Dias <jorge@freshcognate.com>
2025-11-21 15:56:07 -0400

    Fix react warning

--- app/modules/support/components/supportArticles.tsx
+++ app/modules/support/components/supportArticles.tsx
@@ -93,7 +93,10 @@ export default function SupportArticles({
             {map(supportArticles, (supportArticle: SupportArticle) => {
               if (supportArticle.documentId === selectedDocumentId) {
                 return (
-                  <div className="[&_h1]:scroll-m-20 [&_h1]:text-4xl [&_h1]:font-extrabold [&_h1]:tracking-tight [&_h1]:text-balance [&_h1]:mb-4">
+                  <div
+                    key={supportArticle.documentId}
+                    className="[&_h1]:scroll-m-20 [&_h1]:text-4xl [&_h1]:font-extrabold [&_h1]:tracking-tight [&_h1]:text-balance [&_h1]:mb-4"
+                  >
                     <div className="[&_h2]:scroll-m-20 [&_h2]:border-b [&_h2]:pb-2 [&_h2]:text-3xl [&_h2]:font-semibold [&_h2]:tracking-tight [&_h2]:first:mt-0 [&_h2]:mb-2">
                       <div className="[&_p]:leading-7 [&_p]:[&:not(:first-child)]:mb-6">
                         <div className="[&_ol]:my-6 [&_ol]:ml-6 [&_ol]:list-disc [&_ol]:[&>li]:mt-2">
