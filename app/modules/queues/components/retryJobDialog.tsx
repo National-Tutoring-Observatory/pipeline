659d33920042b336a6c9b9961a050c84e02f767f
Jorge Dias <jorge@freshcognate.com>
2025-12-01 15:09:49 -0400

    Failed jobs can be retried
    Fixes #998

---
+++ app/modules/queues/components/retryJobDialog.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import type { Job } from "../queues.types";
+import JobDetailField from "./jobDetailField";
+
+interface RetryJobDialogProps {
+  job: Job;
+  onRetryJobClicked: (jobId: string) => void;
+}
+
+export default function RetryJobDialog({ job, onRetryJobClicked }: RetryJobDialogProps) {
+  const handleRetry = () => {
+    onRetryJobClicked(job.id);
+  };
+
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Retry Job</DialogTitle>
+        <DialogDescription>
+          Are you sure you want to retry this failed job? The job will be re-added to the queue and attempted again.
+        </DialogDescription>
+      </DialogHeader>
+
+      <JobDetailField label="Job Name" value={job.name} />
+
+      <JobDetailField
+        label="Job ID"
+        value={job.id}
+        valueClassName="font-mono"
+      />
+
+      <DialogFooter className="justify-end">
+        <DialogClose asChild>
+          <Button type="button" variant="secondary">
+            Cancel
+          </Button>
+        </DialogClose>
+        <DialogClose asChild>
+          <Button type="button" onClick={handleRetry}>
+            Retry Job
+          </Button>
+        </DialogClose>
+      </DialogFooter>
+    </DialogContent>
+  );
+}
