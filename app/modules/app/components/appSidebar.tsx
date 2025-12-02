403da42a357520be7bfc19fd1944347ea6e2f614
Jorge Dias <jorge@freshcognate.com>
2025-12-02 14:07:31 -0400

    Remove commented code

--- app/modules/app/components/appSidebar.tsx
+++ app/modules/app/components/appSidebar.tsx
@@ -110,15 +110,6 @@ export default function AppSidebar() {
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               </Role>
-              {/* <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <NavLink to={'/users'}>
-                    {({ isActive }) => (
-                      <span className={isActive ? "underline" : ""}>Users</span>
-                    )}
-                  </NavLink>
-                </SidebarMenuButton>
-              </SidebarMenuItem> */}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
@@ -153,10 +144,6 @@ export default function AppSidebar() {
                       size="lg"
                       className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                     >
-                      {/* <Avatar className="h-8 w-8 rounded-lg">
-                        <AvatarImage src={user.avatar} alt={user.username} />
-                        <AvatarFallback className="rounded-lg">{user.username}</AvatarFallback>
-                      </Avatar> */}
                       <div className="grid flex-1 text-left text-sm leading-tight">
                         <span className="truncate font-medium">{user.username}</span>
                         <span className="truncate text-xs">{user.orcidId || user.githubId}</span>
@@ -170,41 +157,6 @@ export default function AppSidebar() {
                     align="end"
                     sideOffset={4}
                   >
-                    {/* <DropdownMenuLabel className="p-0 font-normal">
-                      <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                        <Avatar className="h-8 w-8 rounded-lg">
-                          <AvatarImage src={user.avatar} alt={user.name} />
-                          <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                        </Avatar>
-                        <div className="grid flex-1 text-left text-sm leading-tight">
-                          <span className="truncate font-medium">{user.name}</span>
-                          <span className="truncate text-xs">{user.email}</span>
-                        </div>
-                      </div>
-                    </DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuGroup>
-                      <DropdownMenuItem>
-                        <Sparkles />
-                        Upgrade to Pro
-                      </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuGroup>
-                      <DropdownMenuItem>
-                        <BadgeCheck />
-                        Account
-                      </DropdownMenuItem>
-                      <DropdownMenuItem>
-                        <CreditCard />
-                        Billing
-                      </DropdownMenuItem>
-                      <DropdownMenuItem>
-                        <Bell />
-                        Notifications
-                      </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                    <DropdownMenuSeparator /> */}
                     <DropdownMenuItem onClick={onLogoutClicked}>
                       <LogOut />
                       Log out
