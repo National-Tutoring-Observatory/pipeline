6e75737cd9c9efffb3f0dfbb3a75017c6d3f86a5
darylhedley <darylhedley@hotmail.com>
2025-12-02 14:56:22 +0000

    Adds basic search

--- app/uikit/components/ui/actionBar.tsx
+++ app/uikit/components/ui/actionBar.tsx
@@ -1,6 +1,9 @@
 import map from 'lodash/map';
+import { Search } from 'lucide-react';
 import { useEffect, useRef, useState, type ReactElement } from 'react';
 import { Button } from './button';
+import { ButtonGroup } from './button-group';
+import { Input } from './input';
 
 export type Action = {
   icon?: ReactElement,
@@ -11,13 +14,20 @@ export type Action = {
 
 export type ActionBarProps = {
   actions: Action[]
-  onActionClicked: (action: string) => void
+  searchValue?: string,
+  hasSearch?: boolean,
+  onActionClicked: (action: string) => void,
+  onSearchValueChanged?: (searchValue: string) => void
 }
 
 function ActionBar({
   actions,
-  onActionClicked
+  searchValue,
+  hasSearch = false,
+  onActionClicked,
+  onSearchValueChanged
 }: ActionBarProps) {
+
   const [isStuck, setIsStuck] = useState(false);
   const sentinelRef = useRef<HTMLDivElement | null>(null);
 
@@ -39,12 +49,25 @@ function ActionBar({
       <div ref={sentinelRef} className="h-4" aria-hidden />
 
       <div className={`flex justify-between sticky top-4 border p-2 mb-2 rounded-2xl bg-white ${isStuck ? 'shadow' : ''}`}>
-        <div />
+        <div>
+          {(hasSearch) && (
+            <ButtonGroup >
+              <Input placeholder="Search..." value={searchValue} className="shadow-none" onChange={(event) => {
+                if (onSearchValueChanged) {
+                  onSearchValueChanged(event.target.value);
+                }
+              }} />
+              <Button variant="outline" aria-label="Search" className="shadow-none">
+                <Search />
+              </Button>
+            </ButtonGroup>
+          )}
+        </div>
         <div />
         <div className="flex gap-x-1">
           {map(actions, (action) => {
             return (
-              <Button key={action.action} size={'sm'} onClick={() => onActionClicked(action.action)}>
+              <Button key={action.action} onClick={() => onActionClicked(action.action)}>
                 {action.icon ? action.icon : null}
                 {action.text}
               </Button>
