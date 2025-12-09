8d905173fe40a7937f5eb8cd5b7f349ff0bff907
Jorge Dias <jorge@freshcognate.com>
2025-12-09 09:24:17 -0400

    Scope prompts to user teams

--- app/modules/prompts/containers/promptsList.route.tsx
+++ app/modules/prompts/containers/promptsList.route.tsx
@@ -4,6 +4,8 @@ import getDocumentsAdapter from "~/modules/documents/helpers/getDocumentsAdapter
 import type { Prompt } from "~/modules/prompts/prompts.types";
 import type { User } from "~/modules/users/users.types";
 import type { Route } from "./+types/promptsList.route";
+import getSessionUserTeams from "~/modules/authentication/helpers/getSessionUserTeams";
+import { map } from "lodash";
 
 
 export async function loader({ request, params }: Route.LoaderArgs) {
@@ -11,10 +13,12 @@ export async function loader({ request, params }: Route.LoaderArgs) {
   if (!user) {
     return redirect('/');
   }
+  const authenticationTeams = await getSessionUserTeams({ request });
+  const teamIds = map(authenticationTeams, 'team');
   const url = new URL(request.url);
   const annotationType = url.searchParams.get('annotationType');
   const documents = getDocumentsAdapter();
-  const result = await documents.getDocuments<Prompt>({ collection: 'prompts', match: { annotationType }, sort: {} });
+  const result = await documents.getDocuments<Prompt>({ collection: 'prompts', match: { annotationType, team: { $in: teamIds } }, sort: {} });
   const prompts = { data: result.data };
   return { prompts };
 }
