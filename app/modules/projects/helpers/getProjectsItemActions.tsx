c712fd724beef82603e809a85d425c8d492bfaf6
darylhedley <darylhedley@hotmail.com>
2025-12-17 13:37:04 +0000

    Fixes actions with permissions

--- app/modules/projects/helpers/getProjectsItemActions.tsx
+++ app/modules/projects/helpers/getProjectsItemActions.tsx
@@ -1,15 +1,31 @@
-import type { CollectionItemAction } from "@/components/ui/collectionContentItem"
-import { Edit, Trash2 } from "lucide-react"
+import type { CollectionItemAction } from "@/components/ui/collectionContentItem";
+import { Edit, Trash2 } from "lucide-react";
+import useProjectAuthorization from "../hooks/useProjectAuthorization";
+import type { Project } from "../projects.types";
 
-export default (): CollectionItemAction[] => {
-  return [{
-    action: 'EDIT',
-    icon: <Edit />,
-    text: 'Edit'
-  }, {
-    action: 'DELETE',
-    icon: <Trash2 />,
-    text: 'Delete',
-    variant: 'destructive'
-  }]
+export default (item: Project): CollectionItemAction[] => {
+  const teamId = (item.team as any)._id || item.team;
+  const { canUpdate, canDelete } = useProjectAuthorization(teamId);
+
+  const actions: CollectionItemAction[] = [];
+
+  if (canUpdate) {
+    actions.push({
+      action: 'EDIT',
+      icon: <Edit />,
+      text: 'Edit'
+    });
+  }
+
+  if (canDelete) {
+    actions.push({
+      action: 'DELETE',
+      icon: <Trash2 />,
+      text: 'Delete',
+      variant: 'destructive'
+    })
+  }
+
+  return actions;
+
 }
