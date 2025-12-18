560b784fc2a49a1b250746b0747b5b47abad64cd
darylhedley <darylhedley@hotmail.com>
2025-12-18 13:38:29 +0000

    Adds collections view to add user to team dialog

---
+++ app/modules/teams/helpers/getAddUserToTeamDialogEmptyAttributes.tsx
@@ -0,0 +1,10 @@
+import { Users } from "lucide-react"
+
+export default () => {
+  return {
+    icon: <Users />,
+    title: "No users available",
+    description: "No users available to add to this team",
+    actions: []
+  }
+}
---
+++ app/modules/teams/helpers/getAddUserToTeamDialogItemActions.tsx
@@ -0,0 +1,6 @@
+import type { CollectionItemAction } from "@/components/ui/collectionContentItem";
+import type { User } from "~/modules/users/users.types";
+
+export default (user: User): CollectionItemAction[] => {
+  return [];
+}
