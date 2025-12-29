fd2c346f572d2f019e034faefb059087f9125db8
Jorge Dias <jorge@freshcognate.com>
2025-12-29 11:16:07 -0400

    Split accumulator hook and added tests

--- app/modules/files/containers/uploadFiles.container.tsx
+++ app/modules/files/containers/uploadFiles.container.tsx
@@ -2,38 +2,16 @@ import { useState } from 'react';
 import UploadFiles from "../components/uploadFiles";
 import type { FileType } from '../files.types';
 import getInstructionsByFileType from '../helpers/getInstructionsByFileType';
-
-type AcceptedFile = File & { _id: string };
-
-function getFileKey(f: File) {
-  return `${f.name}-${f.size}-${f.lastModified}`;
-}
+import useFileAccumulator from '../hooks/useFileAccumulator';
 
 interface UploadFilesContainerProps {
   onUploadFiles: ({ acceptedFiles, fileType }: { acceptedFiles: File[], fileType: FileType }) => void;
 }
 
 export default function UploadFilesContainer({ onUploadFiles }: UploadFilesContainerProps) {
-  const [acceptedFiles, setAcceptedFiles] = useState<AcceptedFile[]>([]);
+  const { acceptedFiles, addFiles, removeFile } = useFileAccumulator();
   const [fileType, setFileType] = useState<FileType>('CSV');
   const [isUploading, setIsUploading] = useState(false);
-
-  const onDrop = (droppedFiles: File[]) => {
-    const existingKeys = new Set(acceptedFiles.map((f) => f._id));
-    const newFiles: AcceptedFile[] = [];
-    for (const file of droppedFiles) {
-      const key = getFileKey(file);
-      if (!existingKeys.has(key)) {
-        (file as AcceptedFile)._id = key;
-        newFiles.push(file as AcceptedFile);
-      }
-    }
-    setAcceptedFiles((prev) => [...prev, ...newFiles]);
-  };
-
-  const onDeleteAcceptedFileClicked = (id: string) => {
-    setAcceptedFiles((prev) => prev.filter((f) => f._id !== id));
-  };
 
   const onUploadFilesClicked = () => {
     setIsUploading(true);
@@ -52,8 +30,8 @@ export default function UploadFilesContainer({ onUploadFiles }: UploadFilesConta
       fileType={fileType}
       instructions={getInstructionsByFileType({ fileType })}
       isUploading={isUploading}
-      onDrop={onDrop}
-      onDeleteAcceptedFileClicked={onDeleteAcceptedFileClicked}
+      onDrop={addFiles}
+      onDeleteAcceptedFileClicked={removeFile}
       onUploadFilesClicked={onUploadFilesClicked}
       onFileTypeChanged={onFileTypeChanged}
     />
