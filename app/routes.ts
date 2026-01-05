cff1d115d0b4c278074e54e987caaaa6cb5c1ccc
Jorge Dias <jorge@freshcognate.com>
2026-01-05 14:18:32 -0400

    Create runs inside run creator
    Fixes #1165

--- app/routes.ts
+++ app/routes.ts
@@ -19,6 +19,7 @@ export default [
         id: "COLLECTIONS"
       }),
     ]),
+    route(":projectId/create-run", "modules/projects/containers/projectCreateRun.route.tsx"),
     route(":projectId/runs/:runId", "modules/projects/containers/projectRun.route.tsx"),
     route(":projectId/runs/:runId/sessions/:sessionId", "modules/projects/containers/projectRunSessions.route.tsx"),
     route(":projectId/collections/:collectionId", "modules/projects/containers/projectCollection.route.tsx")
