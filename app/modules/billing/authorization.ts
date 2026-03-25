51c16294e2bab795680db8056c09b04a70c62654
Jorge Dias <jorge@freshcognate.com>
2026-03-25 13:28:31 -0400

    Add billing authorization
    canViewBilling for team members, canManageBilling and canAddCredits for
    super admins and billing users, canSetBillingUser for super admins only.
    Add userIsTeamBillingUser helper.
    
    Fixes #1377

---
+++ app/modules/billing/authorization.ts
@@ -0,0 +1,30 @@
+import { userIsSuperAdmin } from "~/modules/authorization/helpers/superAdmin";
+import { userIsTeamBillingUser } from "~/modules/authorization/helpers/teamBilling";
+import { userIsTeamMember } from "~/modules/authorization/helpers/teamMembership";
+import type { Team } from "~/modules/teams/teams.types";
+import type { User } from "~/modules/users/users.types";
+
+const BillingAuthorization = {
+  canViewBilling(user: User | null, teamId: string): boolean {
+    return userIsTeamMember(user, teamId);
+  },
+
+  canManageBilling(user: User | null, team: Team | null): boolean {
+    return userIsSuperAdmin(user) || userIsTeamBillingUser(user, team);
+  },
+
+  canSetBillingUser(user: User | null): boolean {
+    return userIsSuperAdmin(user);
+  },
+
+  canAddCredits(user: User | null, team: Team | null): boolean {
+    return userIsSuperAdmin(user) || userIsTeamBillingUser(user, team);
+  },
+};
+
+type BillingAuthorizationShape = {
+  [K in keyof typeof BillingAuthorization]: boolean;
+};
+
+export default BillingAuthorization;
+export type { BillingAuthorizationShape };
