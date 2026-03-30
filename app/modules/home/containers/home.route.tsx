e6ab2eda68911ac5d6f4af5db6bc569700af93fc
Jorge Dias <jorge@freshcognate.com>
2026-03-30 18:01:22 -0400

    Redirect / to /projects when OPEN_SIGNUP is not enabled
    Fixes #1832

--- app/modules/home/containers/home.route.tsx
+++ app/modules/home/containers/home.route.tsx
@@ -5,6 +5,8 @@ import Home from "../components/home";
 import type { Route } from "./+types/home.route";
 
 export async function loader({ request }: Route.LoaderArgs) {
+  if (process.env.OPEN_SIGNUP !== "true") return redirect("/projects");
+
   const user = await getSessionUser({ request });
   if (!user) return redirect("/");
 
