77c45ea196fc97425fe05db3bd8088cc56682105
Jorge Dias <jorge@freshcognate.com>
2026-03-30 20:23:54 -0400

    Homepage updates

--- app/modules/home/containers/home.route.tsx
+++ app/modules/home/containers/home.route.tsx
@@ -1,7 +1,5 @@
 import { redirect } from "react-router";
 import getSessionUser from "~/modules/authentication/helpers/getSessionUser";
-import { TeamCreditService } from "~/modules/billing/teamCredit";
-import type { UserTeam } from "~/modules/users/users.types";
 import Home from "../components/home";
 import type { Route } from "./+types/home.route";
 
@@ -9,21 +7,15 @@ export async function loader({ request }: Route.LoaderArgs) {
   if (process.env.OPEN_SIGNUP !== "true") return redirect("/projects");
 
   const user = await getSessionUser({ request });
+  if (!user) return redirect("/signup");
 
-  const teamId = (user?.teams[0] as UserTeam | undefined)?.team;
-
-  const creditBalance = teamId
-    ? await TeamCreditService.sumByTeam(String(teamId))
-    : 0;
-
-  return { creditBalance, userName: user?.name || user?.username || "" };
+  return {};
 }
 
 export function HydrateFallback() {
   return <div>Loading...</div>;
 }
 
-export default function HomeRoute({ loaderData }: Route.ComponentProps) {
-  const { creditBalance, userName } = loaderData;
-  return <Home creditBalance={creditBalance} userName={userName} />;
+export default function HomeRoute() {
+  return <Home />;
 }
