6de1217e8a0b9dfb7b923aecc76048f822df6c80
Jorge Dias <jorge@freshcognate.com>
2026-04-01 13:43:36 -0400

    Implement home view
    Fixes #1831

--- app/modules/home/containers/home.route.tsx
+++ app/modules/home/containers/home.route.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "react-router";
 import getSessionUser from "~/modules/authentication/helpers/getSessionUser";
+import isBillingEnabled from "~/modules/billing/helpers/isBillingEnabled.server";
 import Home from "../components/home";
 import type { Route } from "./+types/home.route";
 
@@ -9,13 +10,13 @@ export async function loader({ request }: Route.LoaderArgs) {
   const user = await getSessionUser({ request });
   if (!user) return redirect("/signup");
 
-  return {};
+  return { billingEnabled: isBillingEnabled() };
 }
 
 export function HydrateFallback() {
   return <div>Loading...</div>;
 }
 
-export default function HomeRoute() {
-  return <Home />;
+export default function HomeRoute({ loaderData }: Route.ComponentProps) {
+  return <Home billingEnabled={loaderData.billingEnabled} />;
 }
