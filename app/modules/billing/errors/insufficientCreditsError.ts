3f04bf820186ffe49d18774f8746a7a53b466560
Jorge Dias <jorge@freshcognate.com>
2026-04-07 14:04:29 -0400

    Link to survey when running out of credits
    Fixes #1947

--- app/modules/billing/errors/insufficientCreditsError.ts
+++ app/modules/billing/errors/insufficientCreditsError.ts
@@ -3,7 +3,7 @@ export class InsufficientCreditsError extends Error {
 
   constructor(teamId: string) {
     super(
-      "Your account has reached its usage limit. Please contact support to continue: https://github.com/National-Tutoring-Observatory/sandpiper/issues/new/choose",
+      "Your account has reached its usage limit. Please contact support to continue: https://forms.gle/e23QwDvQAVZyKD9g8",
     );
     this.name = "InsufficientCreditsError";
     this.teamId = teamId;
