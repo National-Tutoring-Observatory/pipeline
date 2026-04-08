fee5c3e1f0ad3deed32b9c2613fcf3deb61cc3ab
Jorge Dias <jorge@freshcognate.com>
2026-04-08 14:36:35 -0400

    Remove OPEN_SIGNUP feature flag
    Always show open signup flow — remove env var checks and old
    closed-signup login UI.
    
    Fixes #1910

--- app/modules/home/containers/home.route.tsx
+++ app/modules/home/containers/home.route.tsx
@@ -1,9 +1,6 @@
-import { redirect } from "react-router";
 import Home from "../components/home";
-import type { Route } from "./+types/home.route";
 
-export function loader(_: Route.LoaderArgs) {
-  if (process.env.OPEN_SIGNUP !== "true") return redirect("/projects");
+export function loader() {
   return {};
 }
 
